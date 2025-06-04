63774fab77589b7f215945667e437e9952b8c3cd
wimpy <wimpy@wimpys-MacBook-Pro.local>
2025-06-04 15:38:13 -0700

    remove fluff from custmoer page

---
+++ spline.design/_next/static/chunks/pages/enterprise-9c4809de7f6de446.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4949],{79707:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/enterprise",function(){return u(5880)}])}},function(n){n.O(0,[2236,1216,6116,7714,747,6158,9774,2888,179],function(){return n(n.s=79707)}),_N_E=n.O()}]);
